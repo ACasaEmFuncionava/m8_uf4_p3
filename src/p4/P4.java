d91d2a0c8ba67773e608358eb58819864079b2be
ACasaEmFuncionava <randomfary@gmail.com>
2016-04-18 17:30:22 +0200

    add var

--- src/p4/P4.java
+++ src/p4/P4.java
@@ -19,7 +19,7 @@ public class P4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nums = new int[10];
-
+	int a = 1;
         for (int i = 0; i < nums.length; i++) {
             System.out.println("Introduce un valor");
             System.out.print("> ");
